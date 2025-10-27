bf0394f74f6ae9cc7e2e109c52d894d1aed619cf
Durojaiye Ademola Adebayo <ademoladurojaiye15@gmail.com>
2025-10-27 06:13:36 -0700

    Update chain_of_custody.py

--- app/routes/chain_of_custody.py
+++ app/routes/chain_of_custody.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, Depends, UploadFile, File, HTTPException
-from .orm import Session
+from sqlalchemy.orm import Session
 from app import models, schemas, database
 from app.dependencies import get_current_user
 import os
