d951809395682a5837866f324c47553aa2a45ed2
Durojaiye Ademola Adebayo <ademoladurojaiye15@gmail.com>
2025-11-07 19:26:57 +0100

    Update incidents.py

--- app/routes/incidents.py
+++ app/routes/incidents.py
@@ -487,7 +487,7 @@ from sqlalchemy import or_,desc
 from fastapi import HTTPException, Depends
 from sqlalchemy.orm import Session
 from sqlalchemy import or_, desc
-from app import models, schemas, settings
+from app import models, schemas
 from app.dependencies import get_db, get_current_user
 
 async def get_all_incidents_for_regulator(
