cbf382e1522509752ae1317e9222925916253126
Durojaiye Ademola Adebayo <ademoladurojaiye15@gmail.com>
2025-11-08 15:59:27 +0100

    Update incidents.py

--- app/routes/incidents.py
+++ app/routes/incidents.py
@@ -190,7 +190,8 @@ from sqlalchemy.orm import Session
 from sqlalchemy import or_, desc, func
 from typing import List, Optional
 from app import models, schemas
-router.get("/regulator", response_model=List[schemas.IncidentOut])
+
+@router.get("/regulator", response_model=List[schemas.IncidentOut])
 def get_incidents_for_regulator_simple(
     db: Session = Depends(get_db),
     current_user: models.User = Depends(get_current_user)
