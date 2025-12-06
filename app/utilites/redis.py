3b267a7ad8022cca61f45dc8ac6dc9d0b8ffbd6b
Durojaiye Ademola <ademoladurojaiye15@gmail.com>
2025-12-06 20:36:25 +0100

    Update all changed files

--- app/utilites/redis.py
+++
@@ -1,8 +0,0 @@
-import aioredis
-import os
-
-# Redis URL (update if your Redis server is remote or password-protected)
-REDIS_URL = os.getenv("REDIS_URL", "redis://localhost:6379/0")
-
-# Create a Redis client
-redis = aioredis.from_url(REDIS_URL, decode_responses=True) 
