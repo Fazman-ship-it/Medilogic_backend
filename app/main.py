9b5c1984d0e9129033f85aee021a10b4f2dbdfca
Durojaiye Ademola Adebayo <ademoladurojaiye15@gmail.com>
2025-12-19 23:24:28 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -70,7 +70,7 @@ app = FastAPI(
 # ✅ CORS middleware to allow Swagger to send token in headers
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # In production, set this to your frontend domain
+    allow_origins="https://medilogicglobal.vercel.app",  # In production, set this to your frontend domain
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
