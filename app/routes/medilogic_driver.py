ce6531706552854c1b174630ba091eedce37ecc5
Durojaiye Ademola Adebayo <ademoladurojaiye15@gmail.com>
2026-01-23 08:29:44 +0000

    Update medilogic_driver.py

--- app/routes/medilogic_driver.py
+++ app/routes/medilogic_driver.py
@@ -10,10 +10,11 @@ from app import models, schemas
 from app.database import get_db
 from app.auth import get_password_hash, generate_temp_password
 from app.utilites.logging import log_activity
-from app.dependencies import require_role, get_db, get_current_user 
+from app.dependencies import get_current_user 
 from sqlalchemy import case
 import os
 from app.utilites.time_utilities import now_utc
+from app.database import get_db
 
 # Create router for driver endpoints
 router = APIRouter(prefix="/Medilogic_drivers", tags=[" Medilogic_Drivers"])
@@ -153,8 +154,9 @@ def reject_driver(
 
 from fastapi import Depends, HTTPException
 from sqlalchemy.orm import Session
-from app.dependencies import get_db, get_current_user
+from app.dependencies import get_current_user
 from app import models, schemas
+from app.database import get_db
 
 @router.get("/me", response_model=schemas.MedilogicDriverOut)
 def get_my_medilogic_driver_profile(
@@ -185,8 +187,9 @@ from uuid import UUID
 from fastapi import APIRouter, Depends, HTTPException, Query
 from sqlalchemy.orm import Session
 from sqlalchemy import case, cast, String
-from app.dependencies import get_db, get_current_user
+from app.dependencies import get_current_user
 from app import models, schemas
+from app.database import get_db
 
 
 @router.get("/", response_model=List[schemas.MedilogicDriverOut])
@@ -268,7 +271,7 @@ import os
 import stripe
 import uuid
 from app.database import get_db
-from app.dependencies import require_role,get_db, get_current_user 
+from app.dependencies import get_current_user 
 from app import models, schemas
 from app.schemas import SubscriptionPlan, SubscriptionStatus, BadgeType
 from app.utilites.storage_utilites import upload_file_to_s3_async
